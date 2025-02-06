3745fbdb3988a21149c2dbaaf3017cd9553f100c
Aidan McGivern <2897536m@student.gla.ac.uk>
2025-02-06 14:29:23 +0000

    Implemented Chapter 4, Exercise Implemented

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def index(request):
     
     # Construct a dictionary to pass to the template engine as its context.
@@ -16,4 +17,5 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <a href='/rango/'>Index</a>")
+
+    return render(request, 'rango/about.html')
